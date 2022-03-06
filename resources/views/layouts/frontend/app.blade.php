bd2acec51df9141715a472fa28a96204a8273cdd
Osama Roshdy <osama.roshdy@tqinat.com>
2022-03-06 14:05:49 +0200

    cleaning project

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -33,6 +33,20 @@
             @yield('content')
         </main>
 
+        <div class="footer__top">
+            <div class="container">
+                <h4 class="section-title section-title--white">{{ getSection('subscribe')->title }}</h4>
+                <p>{{ getSection('subscribe')->description }}</p>
+                <form class="newsletter_form " method="POST" action="">
+                    <input class="input" type="email" name="email" placeholder="Your Email ..." aria-label="email" autocomplete="off">
+                    <button type="button" class="btn btn-primary custom_button hvr-wobble-vertical">
+                        <img src="{{ asset('frontend/icons/check.png') }}" class="check" alt="">
+                        {{ getSection('subscribe')->button }}
+                    </button>
+
+                </form>
+            </div>
+        </div>
         @include('layouts.frontend.includes.footer')
         @include('layouts.frontend.includes.scripts')
         @yield('scripts')
