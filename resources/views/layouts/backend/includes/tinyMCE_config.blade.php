8ed5dec7e5161cd98d7f8b99cecf9f9565f751f3
Osama Roshdy <osama.roshdy@tqinat.com>
2022-03-06 22:53:48 +0200

    cleaning project

---
+++ resources/views/layouts/backend/includes/tinyMCE_config.blade.php
@@ -0,0 +1,70 @@
+<script>
+    var route_prefix = "{{$target ?? url('/admin-dashboard/filemanager')}}";
+</script>
+<script src="//cdn.tiny.cloud/1/j5wsag0fe55mtznq77wkljlx772wdwqb4gc6pt9cvtvs5a8c/tinymce/4/tinymce.min.js" referrerpolicy="origin"></script><script>
+    var editor_config = {
+        path_absolute : "",
+        selector: ".editor",
+        init_instance_callback : function(editor) {
+            var freeTiny = document.querySelector('.tox .tox-notification--in');
+            freeTiny.style.display = 'none';
+        },
+        theme: 'modern',
+        plugins: [
+            "advlist autolink lists link  charmap  preview hr anchor pagebreak",
+            "searchreplace wordcount visualblocks visualchars code fullscreen",
+            "insertdatetime  nonbreaking save table contextmenu directionality",
+            "emoticons  paste textcolor colorpicker textpattern"
+        ],
+        relative_urls: false,
+        height: 350,
+        templates: [
+            {title: 'Admission Apply', description: 'Admission Apply', url: '{{asset('public/templates/admission_apply.html')}}'},
+
+        ],
+        valid_elements : "@[id|class|style|title|dir<ltr?rtl|lang|xml::lang|onclick|ondblclick|"
+            + "onmousedown|onmouseup|onmouseover|onmousemove|onmouseout|onkeypress|"
+            + "onkeydown|onkeyup],a[rel|rev|charset|hreflang|tabindex|accesskey|type|"
+            + "name|href|target|title|class|onfocus|onblur],strong/b,em,i,strike,u,"
+            + "#p,-ol[type|compact],-ul[type|compact],-li,br,img[longdesc|usemap|"
+            + "src|border|alt=|title|hspace|vspace|width|height|align],-sub,-sup,"
+            + "-blockquote,-table[border=0|cellspacing|cellpadding|width|frame|rules|"
+            + "height|align|summary|bgcolor|background|bordercolor],-tr[rowspan|width|"
+            + "height|align|valign|bgcolor|background|bordercolor],tbody,thead,tfoot,"
+            + "#td[colspan|rowspan|width|height|align|valign|bgcolor|background|bordercolor"
+            + "|scope],#th[colspan|rowspan|width|height|align|valign|scope],caption,-div,"
+            + "-span,-code,-pre,address,-h1,-h2,-h3,-h4,-h5,-section,-h6,hr[size|noshade],-font[face"
+            + "|size|color],dd,dl,dt,cite,abbr,acronym,del[datetime|cite],ins[datetime|cite],"
+            + "object[classid|width|height|codebase|*],param[name|value|_value],embed[type|width"
+            + "|height|src|*],script[src|type],map[name],area[shape|coords|href|alt|target],bdo,"
+            + "button,col[align|char|charoff|span|valign|width],colgroup[align|char|charoff|span|"
+            + "valign|width],dfn,fieldset,form[action|accept|accept-charset|enctype|method],"
+            + "input[accept|alt|checked|disabled|maxlength|name|readonly|size|src|type|value],"
+            + "kbd,label[for],legend,noscript,optgroup[label|disabled],option[disabled|label|selected|value],"
+            + "q[cite],samp,select[disabled|multiple|name|size],small,"
+            + "textarea[cols|rows|disabled|name|readonly],tt,var,big"|"article",
+        toolbar: "undo redo  | bold italic | alignleft aligncenter alignright alignjustify | fontselect | fontsizeselect | bullist numlist outdent indent | code link image media template",
+        fontsize_formats: "8pt 10pt 12pt 14pt 18pt 24pt 36pt",
+        file_browser_callback : function(field_name, url, type, win) {
+            var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+            var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
+
+            var cmsURL = editor_config.path_absolute + route_prefix + '?field_name=' + field_name;
+            if (type == 'image') {
+                cmsURL = cmsURL + "&type=Images";
+            } else {
+                cmsURL = cmsURL + "&type=Files";
+            }
+
+            tinyMCE.activeEditor.windowManager.open({
+                file : cmsURL,
+                title : 'Filemanager',
+                resizable : "yes",
+                close_previous : "no"
+            });
+        }
+    };
+
+    tinymce.init(editor_config);
+</script>
+
