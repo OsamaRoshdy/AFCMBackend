58f0398457adbdf888eb5ed5c150742fc8de293e
Osama Roshdy <osama.roshdy@tqinat.com>
2022-03-16 16:29:34 +0200

    icon loader

--- resources/views/layouts/frontendV2/app.blade.php
+++ resources/views/layouts/frontendV2/app.blade.php
@@ -8,16 +8,62 @@
 
     @yield('styles')
     <link rel="icon" type="image/png" href="assets/images/favicon.png">
+    <style type="text/css">
+        .loader {
+            border: 16px solid #f3f3f3;
+            /* Light grey */
+            border-top: 16px solid var(--optional-color);
+            /* Dark Green */
+            border-radius: 50%;
+            width: 120px;
+            height: 120px;
+            animation: spinloader 2s linear infinite;
+        }
+
+        .loader img {
+            height: 80px;
+            width: 80px;
+            animation: spinlogo 2s linear infinite;
+        }
+
+        @keyframes spinloader {
+            0% {
+                transform: rotate(0deg);
+            }
+
+            100% {
+                transform: rotate(360deg);
+            }
+        }
+
+        @keyframes spinlogo {
+            0% {
+                transform: rotate(360deg);
+            }
+
+            100% {
+                transform: rotate(0deg);
+            }
+        }
+    </style>
     <title>AFCM</title>
 </head>
 
 <body>
 <!-- Start Preloader Area -->
-<div class="preloader">
-    <div class="lds-ripple">
-        <div class="pl-flip-1 pl-flip-2"></div>
+<div class="section-title">
+    <div class="preloader">
+        <div class="lds-ripple">
+            <div class="loader">
+                <img style="margin: 5px" src="{{ asset('frontendV2/assets/images/logo.png') }}" width="50%" />
+            </div>
+        </div>
     </div>
 </div>
+
+{{--<div class="loader">--}}
+{{--    <img src="{{ asset('frontendV2/assets/images/logo.png') }}" />--}}
+{{--</div>--}}
 <!-- End Preloader Area -->
 
 @include('layouts.frontendV2.includes.header')
